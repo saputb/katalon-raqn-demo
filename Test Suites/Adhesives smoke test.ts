174a088979006cc3693d4aed1ad207b276c0ab64
saputb <bobby.saputra0510@gmail.com>
2023-01-13 17:23:34 +0100

    setup config schedule testops run

---
+++ Test Suites/Adhesives smoke test.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Adhesives smoke test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>23cb7f21-c701-4e41-8ad0-6365714237f6</testSuiteGuid>
+   <testCaseLink>
+      <guid>e2448954-0acc-4beb-bd32-6bab1d8c0cc9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/E2E_Test_ADH/Login_Ref_ADH</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
