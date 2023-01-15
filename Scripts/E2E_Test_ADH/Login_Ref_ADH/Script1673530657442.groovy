7e0e9d66bb080af19060b9ccd55a569c4e08546c
saputb <bobby.saputra0510@gmail.com>
2023-01-15 19:12:34 +0100

    update maximize window for safari

--- Scripts/E2E_Test_ADH/Login_Ref_ADH/Script1673530657442.groovy
+++ Scripts/E2E_Test_ADH/Login_Ref_ADH/Script1673530657442.groovy
@@ -19,6 +19,8 @@ import org.openqa.selenium.Keys as Keys
 
 WebUI.openBrowser('')
 
+WebUI.maximizeWindow()
+
 WebUI.navigateToUrl('https://www-beta-henkel-adhesives-com.ref.web.raqn.io/us/en.html/please-log-me-in')
 
 WebUI.verifyElementVisible(findTestObject('Page_Henkel Adhesive Technologies/Text_Cookies_Adhesive'))
