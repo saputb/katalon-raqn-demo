e04fa4d2bc2d99e17eb96d0bf5ec806cc5e9afd5
saputb <bobby.saputra0510@gmail.com>
2023-01-16 10:51:47 +0100

    updated suite

--- Test Suites/Adhesives_SKP login smoke test.ts
+++ Test Suites/Adhesives_SKP login smoke test.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>Adhesives smoke test</name>
+   <name>Adhesives_SKP login smoke test</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
@@ -16,6 +16,13 @@
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/E2E_Test_ADH/Login_Ref_ADH</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>93e7ee6d-57f3-413f-a07c-d9344a3d0f9c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/E2E_Test_SKP/Login_Ref_SKP</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
