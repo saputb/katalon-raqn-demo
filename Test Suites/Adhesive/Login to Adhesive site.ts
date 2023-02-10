84e15b99cd11fa5b6bfcbbb5876878c9335dac50
saputb <bobby.saputra0510@gmail.com>
2023-02-10 12:02:00 +0100

    push test

---
+++ Test Suites/Adhesive/Login to Adhesive site.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login to Adhesive site</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>c78c6897-5d2f-4b97-9c5b-25f38afc47e7</testSuiteGuid>
+</TestSuiteEntity>
